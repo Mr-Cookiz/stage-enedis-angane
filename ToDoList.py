bd505572d3556894a4531afcfd6dc6d66c2941f0
Mr-Cookiz <arthurdenis24@gmail.com>
2024-06-27 16:00:18 +0200

    todolist finis en python

--- ToDoList.py
+++ ToDoList.py
@@ -5,22 +5,28 @@ class ToDoList:
         self.todos = []
     def ajouterTodo (self,todo):
         self.todos.append(todo)
-    def enleverTodo (self,todo):
-        nombre = input("Donner la place du Todo a enlever.")
-        nombre = nombre+1
-        del todolist[nombre]
+    def enleverTodo (self):
+        nombre = int(input("Donner la place du Todo a enlever."))
+        nombre = nombre-1
+        del self.todos[nombre]
     def AfficherTodos (self):
         for todo in self.todos:
             todo.Afficher()
+        print()
     def vider (self):
         self.todos = []
+    def cocherTodo (self,nombre):
+        self.todos[nombre].cocher()
+    def decocherTodo (self,nombre):
+        self.todos[nombre].decocher()
+        
 class Todo:
     def __init__(self):
         self.label = str(input("Donnez un nom au Todo"))
         self.fait = False
     def cocher (self):
         self.fait = True
-    def décocher (self):
+    def decocher (self):
         self.fait = False
     def Afficher (self):
         print(f"{self.label} > {self.fait}")
@@ -29,16 +35,25 @@ todolist = ToDoList()
 
 while True:
     todolist.AfficherTodos()
-    menu = input("1: Ajouter\n2: Enlever\n3: Afficher\n4: Vider\n> ")
+    menu = input("1: Ajouter\n2: Enlever\n3: Afficher\n4: Vider\n5: Cocher\n6: décocher\n")
     if menu == "1":
         todo = Todo()
         todolist.ajouterTodo(todo)
     elif menu == "2":
-        todolist.enleverTodo(todo)
+        todolist.enleverTodo()
     elif menu == "3":
         todolist.AfficherTodos()
+        time.sleep(2)
     elif menu == "4":
         todolist.vider()
+    elif menu == "5":
+        nombre = int(input("Donner la place du Todo a cocher."))
+        nombre = nombre-1
+        todolist.cocherTodo(nombre)
+    elif menu == "6":
+        nombre = int(input("Donner la place du Todo a décocher."))
+        nombre = nombre-1
+        todolist.decocherTodo(nombre)
     else:
         exit()
 
